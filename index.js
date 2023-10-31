b3e0b4b90111f1eb7b939612a18bb3c1ab173011
mdjjdhdh <149448202+mdjjdhdh@users.noreply.github.com>
2023-10-31 23:45:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import menu from "./menu.png";
+import close from "./close.png";
+import logo from './logo.png';
+import search from './search.png';
+import idea from './gif.webp';
+
+export {
+  menu,
+  close,
+  logo,
+  search,
+  idea,
+};
